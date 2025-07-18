e3774884f8b1a765dbca4b903b70210bacbd7812
unknown <gromualdo487@gmail.com>
2025-07-18 15:56:41 -0300

    corrigindo tudo

---
+++ download_parquet.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Baixando o arquivo .parquet..."
+
+curl -L -o dados.parquet "https://store10.gofile.io/download/direct/So3wGW/dados.parquet"
+
+echo "Download finalizado!"
--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+./download_parquet.sh
+
 uvicorn api:app --host=0.0.0.0 --port=10000
